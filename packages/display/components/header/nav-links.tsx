0de54cace2a5b6b024b4a007e9cc02ed09fa0dd4
燃,. <randaoshengxia@gmail.com>
2026-08-20 11:30:43 +0800

    feat: 导航栏根据 figma 更名

--- packages/display/components/header/nav-links.tsx
+++ packages/display/components/header/nav-links.tsx
@@ -12,10 +12,10 @@ export const NavLinks = defineComponent({
   setup() {
     const navLinks: NavLink[] = [
       { label: '城市体检', icon: 'i-ri-focus-3-fill', to: '/display/inspection' },
-      { label: '更新规划', icon: 'i-ri-route-fill rotate-90', to: '/display/plan' },
-      { label: '片区策划', icon: 'i-famicons-folder-open', to: '/display/scheme' },
-      { label: '项目实施', icon: 'i-ri-target-fill', to: '/display/project' },
-      { label: '成果评估', icon: 'i-carbon-result-draft', to: '/display/evaluation' },
+      { label: '前期规划', icon: 'i-ri-route-fill rotate-90', to: '/display/plan' },
+      { label: '投融建运', icon: 'i-famicons-folder-open', to: '/display/scheme' },
+      { label: '征收管理', icon: 'i-ri-target-fill', to: '/display/project' },
+      { label: '名城保护', icon: 'i-ri-ancient-gate-fill', to: '/display/evaluation' },
     ];
 
     return () => (
