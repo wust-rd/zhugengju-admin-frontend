be902e6463359ddcc48827e7c743a882c8978d4a
燃,. <randaoshengxia@gmail.com>
2026-09-03 10:37:27 +0800

    feat: 替换logo背景图,之前的有色差

--- packages/display/components/header/logo.tsx
+++ packages/display/components/header/logo.tsx
@@ -1,13 +1,13 @@
-import { defineComponent } from 'vue';
 import logoBgImg from '@jeesite/assets/images/display/logo-bg.webp';
+import { defineComponent } from 'vue';
 
 export const Logo = defineComponent({
   setup() {
     return () => (
-      <div class="w-360px h-full relative flex items-center justify-center">
-        <img src={logoBgImg} alt="" class="size-full absolute inset-0 z-0" />
+      <div class="w-380px h-full relative flex items-center justify-center">
+        <img src={logoBgImg} alt="" class="h-full absolute inset-0 z-0" />
 
-        <div class="ml-28px text-28px font-900 text-white whitespace-nowrap z-50">武汉市更新综合管理平台</div>
+        <div class="-ml-40px text-28px font-900 text-white whitespace-nowrap z-50">武汉市更新综合管理平台</div>
       </div>
     );
   },
